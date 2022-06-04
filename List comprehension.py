7f470077c47f1f37900ecad803caeca31fd4a10d
Md. Anonto Shuvo <anontoshuvo99@gmail.com>
2022-06-04 21:58:20 +0600

    Create List comprehension.py

---
+++ List comprehension.py
@@ -0,0 +1,24 @@
+# using list comprehension
+
+if __name__ == '__main__':
+    x = int(input())
+    y = int(input())
+    z = int(input())
+    n = int(input())
+    
+    print([[i,j,k] for i in range(x+1) for j in range(y+1) for k in range(z+1) if i+j+k != n])
+    
+# using loop
+
+x = 1
+y = 1
+z = 1
+n = 2
+
+listijk = []
+for i in range(x + 1):
+    for j in range (y + 1):
+        for k in range (z + 1):
+            if i + j + k != n: 
+                listijk.append([i,j,k])
+print(listijk)
