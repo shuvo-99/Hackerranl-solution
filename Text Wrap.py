b18ced305c9e7b2d1cc5fb4b460563be745838dc
Md. Anonto Shuvo <anontoshuvo99@gmail.com>
2022-09-11 23:42:31 +0600

    Create Text Wrap.py

---
+++ Text Wrap.py
@@ -0,0 +1,14 @@
+import textwrap
+
+def wrap(string, max_width):
+    for i in range(max_width,len(string)+max_width,max_width+1):
+
+        string = string[:i]+'\n'+string[i:]
+
+    return string
+    
+
+if __name__ == '__main__':
+    string, max_width = input(), int(input())
+    result = wrap(string, max_width)
+    print(result)
