1fe8cdb48717ffef0e9aa9ee225e6684ab00a7fe
Md. Anonto Shuvo <anontoshuvo99@gmail.com>
2022-09-18 23:08:26 +0600

    Create String Formatting.py

---
+++ String Formatting.py
@@ -0,0 +1,12 @@
+def print_formatted(number):
+    length = len(str(bin(number))) - 2
+    for i in range(1,number+1):
+        a = str(i)
+        b = str(oct(i))
+        c = str(hex(i).upper())
+        d = str(bin(i))
+            
+        print(a.rjust(length, " "),b[2:].rjust(length, " "),c[2:].rjust(length, " "),d[2:].rjust(length," "))
+if __name__ == '__main__':
+    n = int(input())
+    print_formatted(n)
